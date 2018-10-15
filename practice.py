c2a882ebed1b0c9cea4bc9ec2abe7452d23be044
roohom <roohom@qq.com>
2018-10-15 19:54:09 +0800

    for examole: 153 = 1**3 + 5**3 + 3**3

--- practice.py
+++ practice.py
@@ -4,17 +4,43 @@
 # Author: roohom
 # Date  : 2018/9/28 0028
 
+# 找出所有的水仙花数 例如:153 = 1^3+3^3+5^3
 
-#递归生成器
 
-def flatten(nested):
-    try:
-        for sublist in nested:
-            for element in  flatten(sublist):
-                yield element
-    except TypeError:
-        yield nested
+# 字符串追加
+
+
+def str_plus(list):
+    l = [str(i) for i in list]
+    string = "".join(l)
+    return string
+
+
+def find_daffodil(list):
+    while True:
+        str_plus(list)
+        return str_plus(list)
+
+
+# 将一个数拆分成各个位数数字并取其三次方之和
+def pos_sum(s):
+    m = 0
+    n = 0
+    sum = 0
+
+    for z in range(len(s)):
+        x = int(z)
+        sum = sum + int(s[x])**3
+    return sum
+
 
 if __name__ == '__main__':
-    s = list(flatten([[[1],2],3,4,[5,[6,7]],8]))
-    print(s)
+    i = 0
+    y = input("请输入你想查找水仙花数的范围：")
+    p = int(y)
+    for j in range(p):
+        list = [i]
+        s = find_daffodil(list)
+        if pos_sum(s) == i :
+            print("找到啦！这是一个水仙花数：",i)
+        i += 1
--- test.py
+++ test.py
@@ -1,72 +1,28 @@
-__author__ = 'roohom'
-# -*-coding:utf-8-*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File  : test.py
+# Author: roohom
+# Date  : 2018/10/10 0010
 
 
 
 
-from tkinter import *
-from random import randint
+def upstairs(step):
+    if (step % 2 == 1) & (step % 3 == 2) & (step % 4 == 3) & (step % 5 == 4) & (step % 6 == 5) & (step % 7 == 0):
+        print("这个数是：",step)
+        return True
+    else:
+        return False
 
 
-class RandomBall(object):
-    def __init__(self, canvas, screenwidth, screenheight):
-        self.canvas = canvas
-        self.xpos = randint(10, int(screenwidth))
-        self.ypos = randint(10, int(screenheight))
-        self.xspeed = randint(6, 12)
-        self.yspeed = randint(6, 12)
-        self.screenwidth = screenwidth
-        self.screenheight = screenheight
-        self.radius = randint(40, 70)
-        color = lambda: randint(0, 255)
-        self.color = '#%02x%02x%02x' % (color(), color(), color())
+if __name__ == '__main__':
+    step = 0
+    while True:
+        upstairs(step)
+        step += 1
+        if upstairs(step):
+            print("找到了！")
+            break
+        else:
+            print("执行到我了！")
 
-    def create_ball(self):
-        x1 = self.xpos - self.radius
-        y1 = self.ypos - self.radius
-        x2 = self.xpos + self.radius
-        y2 = self.ypos + self.radius
-        self.itm = self.canvas.create_oval(x1, y1, x2, y2, fill=self.color,
-                                           outline=self.color)
-
-    def move_ball(self):
-        self.xpos += self.xspeed
-        self.ypos += self.yspeed
-        if self.ypos >= self.screenheight - self.radius:
-            self.yspeed = -self.yspeed
-        if self.ypos <= self.radius:
-            self.yspeed = abs(self.yspeed)
-        if self.xpos >= self.screenwidth - self.radius or self.xpos <= self.radius:
-            self.xspeed = -self.xspeed
-        self.canvas.move(self.itm, self.xspeed, self.yspeed)
-
-
-class ScreenSaver:
-    def __init__(self, num_balls):
-        self.balls = []
-        self.root = Tk()
-        w, h = self.root.winfo_screenwidth(), self.root.winfo_screenheight()
-        self.root.overrideredirect(1)
-        self.root.attributes('-alpha', 0.3)
-        self.root.bind('<Key>', self.myquit)
-        self.root.bind('<Motion>', self.myquit)
-        self.canvas = Canvas(self.root, width=w, height=h)
-        self.canvas.pack()
-        for i in range(num_balls):
-            ball = RandomBall(self.canvas, screenwidth=w, screenheight=h)
-            ball.create_ball()
-            self.balls.append(ball)
-        self.run_screen_saver()
-        self.root.mainloop()
-
-    def run_screen_saver(self):
-        for ball in self.balls:
-            ball.move_ball()
-        self.canvas.after(50, self.run_screen_saver)
-
-    def myquit(self, event):
-        self.root.destroy()
-
-
-if __name__ == "__main__":
-    ScreenSaver(18)
